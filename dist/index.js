44e57c0b1ccb761eeb95a609f6220799ecaf877c
Dmytro <73442802+antitoxic316@users.noreply.github.com>
2023-12-22 02:59:39 +0100

    installed modules for building local server and tensorflow and transferred everything on local server

---
+++ dist/index.js
@@ -0,0 +1,42 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
+
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    port = process.argv[2] || 8888;
+
+http.createServer(function (request, response) {
+
+  var uri = url.parse(request.url).pathname,
+      filename = path.join(process.cwd(), uri);
+
+  fs.exists(filename, function (exists) {
+    if (!exists) {
+      response.writeHead(404, { "Content-Type": "text/plain" });
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
+
+    if (fs.statSync(filename).isDirectory()) filename += './drawer.html';
+
+    fs.readFile(filename, "binary", function (err, file) {
+      if (err) {
+        response.writeHead(500, { "Content-Type": "text/plain" });
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      response.writeHead(200);
+      response.write(file, "binary");
+      response.end();
+    });
+  });
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+
+},{"fs":undefined,"http":undefined,"path":undefined,"url":undefined}]},{},[1]);
